3f4dbd568345c8f0a2c68038f3da8b1bf53bc069
Robert <22194161+robert-leitl@users.noreply.github.com>
2022-05-30 13:58:41 +0200

    init empty app

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# paperclips
+# Paperclips
+
+![Paperclips Screenshot](https://github.com/robert-leitl/paperclips/blob/main/cover.jpg?raw=true)
+
+Physics simulation using ammo.js and native webgl rendering.
+
+[DEMO](https://robert-leitl.github.io/paperclips/dist/?debug)
+
+### Features
+- [ammo.js](https://kripken.github.io/ammo.js/) physics engine
---
+++ src/paperclips.js
@@ -0,0 +1,217 @@
+import { mat4, quat, vec2, vec3 } from 'gl-matrix';
+import { createAndSetupTexture, createFramebuffer, createProgram, makeBuffer, makeVertexArray, resizeCanvasToDisplaySize, setFramebuffer } from './utils/webgl-utils';
+
+export class Paperclips {
+    oninit;
+
+    #time = 0;
+    #frames = 0;
+    #deltaTime = 0;
+    #isDestroyed = false;
+
+    camera = {
+        matrix: mat4.create(),
+        near: 60,
+        far: 155,
+        distance: 100,
+        orbit: quat.create(),
+        position: vec3.create(),
+        rotation: vec3.create(),
+        up: vec3.fromValues(0, 1, 0)
+    };
+
+    animate = true;
+
+    constructor(canvas, pane, oninit = null) {
+        this.canvas = canvas;
+        this.pane = pane;
+        this.oninit = oninit;
+
+        this.#init();
+    }
+
+    resize() {
+        const gl = this.gl;
+
+        const needsResize = resizeCanvasToDisplaySize(gl.canvas);
+        
+        if (needsResize) {
+            gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+            this.#resizeTextures(gl);
+        }
+
+        this.#updateProjectionMatrix(gl);
+    }
+
+    run(time = 0) {
+        if(this.fpsGraph) this.fpsGraph.begin();
+
+        this.#deltaTime = Math.min(32, time - this.#time);
+        this.#time = time;
+
+        if (this.animate)
+            this.#frames += this.#deltaTime / 16;
+
+        if (this.#isDestroyed) return;
+
+        this.#render();
+
+        if(this.fpsGraph) this.fpsGraph.end();
+
+        requestAnimationFrame((t) => this.run(t));
+    }
+
+    #render() {
+        /** @type {WebGLRenderingContext} */
+        const gl = this.gl;
+    }
+
+    destroy() {
+        this.#isDestroyed = true;
+    }
+
+    #init() {
+        this.gl = this.canvas.getContext('webgl2', { antialias: false, alpha: false });
+
+        /** @type {WebGLRenderingContext} */
+        const gl = this.gl;
+
+        if (!gl) {
+            throw new Error('No WebGL 2 context!')
+        }
+
+        if (!gl.getExtension("EXT_color_buffer_float")) {
+            console.error("FLOAT color buffer not available");
+            document.body.innerHTML = "This example requires EXT_color_buffer_float which is unavailable on this system."
+        }
+
+        ///////////////////////////////////  PROGRAM SETUP
+
+        // setup programs
+        
+        // find the locations
+        
+        
+        // setup uniforms
+        this.drawUniforms = {
+            worldMatrix: mat4.create(),
+            viewMatrix: mat4.create(),
+            cameraMatrix: mat4.create(),
+            projectionMatrix: mat4.create(),
+            inversProjectionMatrix: mat4.create(),
+            worldInverseTransposeMatrix: mat4.create()
+        };
+
+        /////////////////////////////////// GEOMETRY / MESH SETUP
+
+        // create quad VAO
+        const quadPositions = [-1, -1, 3, -1, -1, 3];
+        this.quadBuffers = {
+            position: makeBuffer(gl, new Float32Array(quadPositions), gl.STATIC_DRAW),
+            numElem: quadPositions.length / 2
+        };
+        this.quadVAO = makeVertexArray(gl, [[this.quadBuffers.position, 0, 2]]);
+
+        /////////////////////////////////// FRAMEBUFFER SETUP
+
+        // initial client dimensions
+        const clientSize = vec2.fromValues(gl.canvas.clientWidth, gl.canvas.clientHeight);
+        this.drawBufferSize = vec2.clone(clientSize);
+
+        this.resize();
+
+        this.camera.position[2] = this.camera.distance;
+        this.#updateCameraMatrix();
+        this.#updateProjectionMatrix(gl);
+
+        this.initTweakpane();
+
+        if (this.oninit) this.oninit(this);
+    }
+
+    #resizeTextures(gl) {
+        const clientSize = vec2.fromValues(gl.canvas.clientWidth, gl.canvas.clientHeight);
+        this.drawBufferSize = vec2.clone(clientSize);
+        
+        /*this.#resizeTexture(gl, this.plantColorTexture, gl.RGBA, clientSize);
+        this.#resizeTexture(gl, this.plantDepthTexture, gl.DEPTH_COMPONENT32F, clientSize);
+        this.#resizeTexture(gl, this.deltaDepthColorTexture, gl.RGBA, clientSize);
+        this.#resizeTexture(gl, this.hBlurTexture, gl.RGBA, clientSize);
+        this.#resizeTexture(gl, this.vBlurTexture, gl.RGBA, clientSize);*/
+
+        // reset bindings
+        gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+        gl.bindTexture(gl.TEXTURE_2D, null);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    }
+
+    #resizeTexture(gl, texture, format, size) {
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+
+        if (format === gl.RGBA) 
+            gl.texImage2D(gl.TEXTURE_2D, 0, format, size[0], size[1], 0, format, gl.UNSIGNED_BYTE, null);
+        else if (format === gl.DEPTH_COMPONENT32F) 
+            gl.texImage2D(gl.TEXTURE_2D, 0, format, size[0], size[1], 0, gl.DEPTH_COMPONENT, gl.FLOAT, null);
+
+        gl.bindTexture(gl.TEXTURE_2D, null);
+    }
+
+    #updateCameraMatrix() {
+        mat4.targetTo(this.camera.matrix, this.camera.position, [0, 0, 0], this.camera.up);
+        mat4.invert(this.drawUniforms.viewMatrix, this.camera.matrix);
+        mat4.copy(this.drawUniforms.cameraMatrix, this.camera.matrix);
+    }
+
+    #updateProjectionMatrix(gl) {
+        const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+        mat4.perspective(this.drawUniforms.projectionMatrix, Math.PI / 4, aspect, this.camera.near, this.camera.far);
+    }
+
+    initTweakpane() {
+        if (this.pane) {
+            const maxFar = 200;
+
+            this.fpsGraph = this.pane.addBlade({
+                view: 'fpsgraph',
+                label: 'fps',
+                lineCount: 1,
+                maxValue: 120,
+                minValue: 0
+            });
+
+            //this.pane.addInput(this, 'animate', { label: 'animate plant' });
+
+            //const cameraFolder = this.pane.addFolder({ title: 'Camera' });
+            //this.#createTweakpaneSlider(cameraFolder, this.camera, 'near', 'near', 1, maxFar, null, () => this.#updateProjectionMatrix(this.gl));
+            //this.#createTweakpaneSlider(cameraFolder, this.camera, 'far', 'far', 1, maxFar, null, () => this.#updateProjectionMatrix(this.gl));
+
+            /*const particlesFolder = this.pane.addFolder({ title: 'Particles' });
+            this.#createTweakpaneSlider(particlesFolder, this.particles.settings, 'velocity', 'velocity', 0, 10, null);
+            this.#createTweakpaneSlider(particlesFolder, this.particles.settings, 'curl', 'curl', 0, 10, null);
+            this.#createTweakpaneSlider(particlesFolder, this.particles.settings, 'noise', 'noise', 0, 10, null);
+
+            const refractionFolder = this.pane.addFolder({ title: 'Refraction' });
+            this.#createTweakpaneSlider(refractionFolder, this.refractionSettings, 'strength', 'strength', 0, 1, null);
+            this.#createTweakpaneSlider(refractionFolder, this.refractionSettings, 'dispersion', 'dispersion', 0, 10, null);*/
+
+            //const plantFolder = this.pane.addFolder({ title: 'Plant' });
+            /*const plantGenerateBtn = plantFolder.addButton({ title: 'generate' });
+            plantGenerateBtn.on('click', () => this.plant.generate(this.#frames));*/
+        }
+    }
+
+    #createTweakpaneSlider(folder, obj, propName, label, min, max, stepSize = null, callback) {
+        const slider = folder.addBlade({
+            view: 'slider',
+            label,
+            min,
+            max,
+            step: stepSize,
+            value: obj[propName],
+        });
+        slider.on('change', e => {
+            obj[propName] = e.value;
+            if(callback) callback();
+        });
+    }
+}
